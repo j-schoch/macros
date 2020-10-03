c85074e65dcc88964e2ff7301c3c0e42da76b7ad
Jeff <shocktastic777@gmail.com>
2020-10-03 03:52:46 -0400

    Create announce_round_number.js

---
+++ misc/announce_round_number.js
@@ -0,0 +1,2 @@
+let messageContent = `<hr><div style="font-size: 1.25em; font-family: 'Signika'; font-weight: bold; margin: 0px; padding: 0px;">ROUND ${game.combat.round}</div><hr>`
+ChatMessage.create({content: messageContent});
